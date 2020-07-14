5b173df8668b59dfaf6085d24526afd1bf941173
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-07-14 14:35:36 -0500

    Add files via upload

---
+++ eggtimer.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+let endTime = 179
+
+
+
+
+
+
+
+for index in 0...179 {
+    print("\(index + 1)")
+    if index >= endTime {
+        print("your egg is ready")
+
+    }
+    if index == 149 {
+        print("Thirty second til your egg is done")
+    }
+}
+
+
